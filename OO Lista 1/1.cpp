d536a91f6dc79a9fa6edbf1fbe015e470773d0df
BlimblimCFT <galefreitas@gmail.com>
2022-06-26 13:38:02 -0300

    First Exercise

--- OO Lista 1/1.cpp
+++ OO Lista 1/1.cpp
@@ -1,1 +1,10 @@
 #include <iostream>
+using namespace std;
+
+int main() {
+    char crc;
+    cout << "Digite um caractere: \n";
+    cin >> crc;
+    cout << "Caractere digitado: \n" << crc << endl << "Codigo ASCII: \n" << int(crc);
+    return 0; 
+ }
