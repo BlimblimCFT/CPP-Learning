7a14bf5b2f168df4917b525314a5731ef35cccda
BlimblimCFT <galefreitas@gmail.com>
2022-06-26 17:19:51 -0300

    Exercise 4 With Input Validation

--- OO Lista 1/4.cpp
+++ OO Lista 1/4.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main(){
     int n, ini=1, soma=0;
-    cout << "Somatorio quadratico para 1 ate: \n";
-    cin >> n;
+    while (cout << "Somatorio quadratico para 1 ate: \n" && (!(cin >> n) || n <=1))
+    {
+        cout << "O numero digitado nao eh um numero ou nao eh maior que 1!\n";
+        cin.clear();
+        cin.ignore();
+    } 
     while (ini<=n)
     {
         soma += ini*ini;
