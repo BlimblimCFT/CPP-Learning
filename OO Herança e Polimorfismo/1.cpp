772f7cd62df6aa1685c72e0a121b2ba5e35434c7
Blimblim <galefreitas@gmail.com>
2022-08-23 22:27:56 -0300

    Add Exercicio de Herança(nao feito)

---
+++ OO Herança e Polimorfismo/1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+//Existem classes bases e classes derivadas. Para definir uma classe derivada, usamos class NomeSubClasse : modificador NomeClasseBase.
+//o modificador pode ser public, protected ou private.
+//Faça uma classe veiculorodoviario e subclasses caminhao e automovel
+class Funcionario{
+};
+
+class Gerente {
+};
